9a97331eb12c6528b4e896cb6d06ffa734c90551
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-10-11 14:31:57 -0600

    improved search function / added comments

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
+//creates the search field
 function searchField() {
-  // creates search field in header
   var header = document.querySelector("header");
   header.insertAdjacentHTML(
     "beforeend",
@@ -13,87 +13,23 @@ function searchField() {
   );
 }
 
-//assembles chunks of names for comparison to user input
-function getChunks(parentArr, name) {
-  chunk = "";
-  for (let i = 0; i < name.length; i++) {
-    chunk += name[i];
-    parentArr.push(chunk);
-  }
-  return parentArr;
-}
-
-//populates arrays of name chunks for comparison to user input using .includes();
-function convertNameToArr(firstname, lastname) {
-  let nameArr = [];
-  let first = firstname.toLowerCase().split("");
-  let last = lastname.toLowerCase().split("");
-  let full = firstname.toLowerCase() + " " + lastname.toLowerCase();
-
-  getChunks(nameArr, first);
-  getChunks(nameArr, last);
-  getChunks(nameArr, full);
-
-  for (let i = 0; i < full.length; i++) {
-    nameArr.push(full[i]);
-  }
-
-  return nameArr;
-}
-
-//compares user input names using aforementioned converNameToArr() function
+//filters data against search input
 function compareNamesToInput(input) {
-  let convertedName = "";
-  let newData = [];
+  let filteredObjects = [];
   for (let i = 0; i < 42; i++) {
-    let first = data[i].name.first;
-    let last = data[i].name.last;
-    convertedName = convertNameToArr(first, last);
-    if (convertedName.includes(input)) {
-      newData.push(data[i]);
+    let first = data[i].name.first.toLowerCase();
+    let last = data[i].name.last.toLowerCase();
+    let full = first + " " + last;
+
+    if (full.includes(input)) {
+      filteredObjects.push(data[i]);
     }
   }
-  return newData;
+  return filteredObjects;
 }
 
-//search box listener - fires on each input event
-function search() {
-  const input = document.getElementById("search");
-  let original = sortStudents(9, data);
-  let pageDiv = document.getElementsByClassName("pagination")[0];
-  pageDiv.insertAdjacentHTML("beforeend", `<ul id="link-list-2"></ul>`);
-  let linkList2 = document.getElementById("link-list-2");
-  linkList2.style.display = "none";
-  presentStudents(original);
-  listen(linkList, original);
-
-  input.addEventListener("input", () => {
-    let userInput = input.value.toLowerCase();
-    let filteredArr = compareNamesToInput(userInput); //returns filtered data from user input
-    let sorted = sortStudents(9, filteredArr);
-    pageDiv.style.display = "block";
-
-    if (input.value === "") {
-      linkList.style.display = "block";
-      linkList2.style.display = "none";
-      presentStudents(original);
-    } else if (sorted.length === 1) {
-      presentStudents(sorted);
-      pageDiv.style.display = "none";
-    } else if (sorted.length < 1) {
-      studentList.innerHTML = `<p class="no-results">no dice, dude.<p>`;
-      pageDiv.style.display = "none";
-    } else {
-      presentStudents(sorted);
-      linkList.style.display = "none";
-      linkList2.style.display = "block";
-      makeButtons(linkList2, sorted);
-      listen(linkList2, sorted);
-    }
-  });
-}
-
-function listen(parent, arr) {
+//page button listener, called back within search();
+function pageButtonListen(parent, arr) {
   parent.children[0].children[0].className = "active";
   //listens to ul
   parent.addEventListener("click", (e) => {
@@ -111,5 +47,92 @@ function listen(parent, arr) {
   });
 }
 
-searchField();
+//search box listener - fires on each input event
+function search() {
+  //displays search field
+  searchField();
+
+  //selects search field
+  const input = document.getElementById("search");
+
+  //intial sort without filtering
+  let originalSortedArr = sortStudents(9, data);
+
+  /* 
+      After encountering problems with duplicate event listeners, I 
+      decided to insert a second button menu below with it's own
+      listener profile.  The code below seems like it ought not
+      be so long, so I'm eager to get feedback on how to achieve
+      greater concision.
+  */
+
+  //dynamic insertion of second button ul
+  let pageDiv = document.getElementsByClassName("pagination")[0];
+  pageDiv.style.display = "block";
+  pageDiv.insertAdjacentHTML("beforeend", `<ul id="link-list-2"></ul>`);
+
+  //selecting second button ul
+  let linkList2 = document.getElementById("link-list-2"); //secondary button set
+  linkList2.style.display = "none";
+
+  //initial presentation and page button listening
+  presentStudents(originalSortedArr);
+  pageButtonListen(linkList, originalSortedArr);
+
+  //input field listener with callback
+  input.addEventListener("input", () => {
+    let userInput = input.value.toLowerCase();
+    let filteredArr = compareNamesToInput(userInput); //returns filtered data from user input
+    let sorted = sortStudents(9, filteredArr); //sorts filtered data
+
+    /* 
+    
+        I attempted to move this callback to a seperate function
+        declaration, but was unsuccessful, as I encountered numerous
+        problems related to variable scope.  Thus, I am not satsified
+        with this code as it stands, but, on the bright side,
+        it does seem to work.
+      
+    */
+
+    if (input.value === "") {
+      //restores originalSortedArr presentation upon instance of blank input field
+      linkList.style.display = "block";
+      linkList2.style.display = "none";
+      presentStudents(originalSortedArr);
+    } else if (sorted.length === 1) {
+      //hides buttons if there is only 1 page
+      linkList.style.display = "none";
+      linkList2.style.display = "none";
+    } else if (sorted.length < 1) {
+      //no results
+      let noResults = noDice[Math.floor(Math.random() * noDice.length)];
+      studentList.innerHTML = `<p class="no-results">${noResults}</p>`;
+      linkList.style.display = "none";
+      linkList2.style.display = "none";
+    } else {
+      //filtered results with new page buttons
+      presentStudents(sorted);
+      linkList.style.display = "none"; //hides first ul set
+      linkList2.style.display = "block"; //reveals second ul
+      makeButtons(linkList2, sorted); //constructs second ul
+      pageButtonListen(linkList2, sorted); //creates listeners for second ul
+    }
+  });
+}
+
 search();
+
+const noDice = [
+  "We got nothing.",
+  "Sorry.  Notta.",
+  "They aren't here.",
+  "No dice, friend.",
+  "These are not the droids you're looking for.",
+  "You're looking for who?",
+  "They've been sacked.",
+  "Oops.  Try again.",
+  "Are any of us really here?",
+  "Uh oh...",
+  "They've gone fishing.",
+];
